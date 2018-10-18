4aac895f95bea5c6923a461e4ae5635beeed14e0
Aleksandr Repeckiy <repetskiyaleksandr@gmail.com>
2018-10-18 16:37:36 +0300

    add paralax and align text on svg

--- app/js/script.js
+++ app/js/script.js
@@ -31,6 +31,30 @@ $(document).ready(function() {
     });
 });
 
+$(window).scroll(function () {
+    var wScroll = $(window).scrollTop();
+    (function () {
+        
+        var
+            bg = $('.hero__bg'),
+            sectionText = $('.section__title__pic'),
+            user = $('.user');
+
+            slideIt(bg, wScroll/45);
+            slideIt(sectionText, wScroll/15);
+            slideIt(user, wScroll/3);
+
+        function slideIt(block, strafeAmount) {
+            var strafe = -strafeAmount+'%',
+                transformString = 'translate3d(0,'+strafe +',0)';
+            block.css({
+                'transform' : transformString
+            });
+        }
+
+    }());
+});
+
 // /**
 //  * @module preloader
 //  */
